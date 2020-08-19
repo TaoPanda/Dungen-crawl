5cd33ce9cda7bae878be8e8a471418c46d0e0739
TaoPanda <62134703+TaoPanda@users.noreply.github.com>
2020-08-19 09:56:04 +0200

    Add project files.

---
+++ Dungen crawl/Enemy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dungen_crawl
+{
+    class Enemy
+    {
+    }
+}
---
+++ Dungen crawl/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dungen_crawl
+{
+    class Player
+    {
+        private int str;
+        private int dex;
+        private int wis;
+        private int con;
+        private int exp;
+        private int level;
+        private int sp;
+        private int hp;
+
+        public Player(int str, int dex, int wis, int con, int exp)
+        {
+            this.str = str;
+            this.dex = dex;
+            this.wis = wis;
+            this.con = con;
+            this.exp = exp;
+            level = 1;
+            sp = 10;
+        }
+
+        public int Str { get => str; set => str = value; }
+        public int Dex { get => dex; set => dex = value; }
+        public int Wis { get => wis; set => wis = value; }
+        public int Con { get => con; set => con = value; }
+        public int Exp { get => exp; set => exp = value; }
+        public int Level { get => level; set => level = value; }
+        public int SP { get => sp; set => sp = value; }
+        public int Hp { get => hp; set => hp = value; }
+
+
+    }
+}
